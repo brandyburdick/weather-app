3020c94fce7ce3b2e3c46f8fd48cd51f61209fc3
Brandy Burdick <blburdic@asu.edu>
2016-08-22 10:49:20 -0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  get 'welcome/test'
-
   root 'welcome#test'
+
+  get  'index' => 'welcome#index'
+  post 'index' => 'welcome#index'
+  get  'test'  => 'welcome#test'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
